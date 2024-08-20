bdf4e212b9d9c212f288671b14d36e097574b3e3
desmoris <damianesmoris@gmail.com>
2024-08-20 07:54:47 -0300

    fing qol module added with command: ArreglarFormatoDeLosPracticos...

---
+++ lua/myconf/myplugins/fing-qol.lua
@@ -0,0 +1,13 @@
+
+return {
+	{
+		name="ArreglarFormatoDeLosPracticosYElLatexDeLosCojone",
+		callback=function()
+			vim.cmd [[call append(line('.')-1, '---')]]
+			vim.cmd [[call append(line('.')+2, '---')]]
+			vim.cmd [[%s:^p´ag. [1-9] de [1-5]:]]
+			vim.cmd [[%s:´a:á:g | %s:´e:é:g | %s:´ı:í:g | %s:´o:ó:g | %s:´u:ú:g]]
+		end,
+		args={ nargs = '*', desc = 'Run the whole file' }
+	}
+}
--- lua/myconf/utils.lua
+++ lua/myconf/utils.lua
@@ -1,4 +1,17 @@
-local commands = require 'myconf.myplugins.runcode'
+local function copy_table(t)
+  local u = { } for k, v in pairs(t) do u[k] = v end
+  return setmetatable(u, getmetatable(t))
+end
+
+local function concat_table_w_keys(t1,t2)
+	local new_table = copy_table(t1)
+	for key,value in pairs(t2) do new_table[key] = value end
+	return new_table
+end
+
+local commands = {}
+commands = concat_table_w_keys(commands, require 'myconf.myplugins.runcode')
+commands = concat_table_w_keys(commands, require 'myconf.myplugins.fing-qol')
 
 for _,command in pairs(commands) do
 	vim.api.nvim_create_user_command(command["name"], command["callback"], command["args"])

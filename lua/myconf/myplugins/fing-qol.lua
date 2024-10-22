33c9d4a96c6794d0e8f5eb36191fd19171084733
desmoris <damianesmoris@gmail.com>
2024-10-21 23:12:21 -0300

    fing-qol ñ added to the replace expresion

--- lua/myconf/myplugins/fing-qol.lua
+++ lua/myconf/myplugins/fing-qol.lua
@@ -6,7 +6,7 @@ return {
 			vim.cmd [[call append(line('.')-1, '---')]]
 			vim.cmd [[call append(line('.')+2, '---')]]
 			vim.cmd [[%s:^p´ag. [1-9] de [1-5]:]]
-			vim.cmd [[%s:´a:á:g | %s:´e:é:g | %s:´ı:í:g | %s:´o:ó:g | %s:´u:ú:g]]
+			vim.cmd [[%s:´a:á:g | %s:´e:é:g | %s:´ı:í:g | %s:´o:ó:g | %s:´u:ú:g | %s:˜n:ñ:g]]
 		end,
 		args={ nargs = '*', desc = 'Run the whole file' }
 	}

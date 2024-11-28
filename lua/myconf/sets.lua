2e54a598863b0a11c0fc4dfde2d71d516117762c
desmoris <damianesmoris@gmail.com>
2024-11-28 13:24:05 -0300

    tabs -> 4 spaces

--- lua/myconf/sets.lua
+++ lua/myconf/sets.lua
@@ -14,9 +14,10 @@ opt.number = true;
 opt.relativenumber = true;
 
 -- QoL
-opt.tabstop = 2;
-opt.shiftwidth = 2;
-opt.softtabstop = 2;
+opt.tabstop = 4;
+opt.shiftwidth = 4;
+opt.softtabstop = 4;
+opt.expandtab = true;
 opt.wrap = true;
 
 -- netrtw 

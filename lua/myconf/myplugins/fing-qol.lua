e43fe086ea706aaf3c95c3e6adb429305c1be9b7
desmoris <damianesmoris@gmail.com>
2025-04-07 09:21:59 -0300

    git-qol fixed

--- lua/myconf/myplugins/fing-qol.lua
+++ lua/myconf/myplugins/fing-qol.lua
@@ -3,11 +3,15 @@ return {
 	{
 		name="ArreglarFormatoDeLosPracticosYElLatexDeLosCojone",
 		callback=function()
-			vim.cmd [[call append(line('.')-1, '---')]]
-			vim.cmd [[call append(line('.')+2, '---')]]
-			vim.cmd [[%s:^p´ag. [1-9] de [1-5]:]]
-			vim.cmd [[%s:´a:á:g | %s:´e:é:g | %s:´ı:í:g | %s:´o:ó:g | %s:´u:ú:g | %s:˜n:ñ:g]]
+			vim.cmd [[
+                silent! %s:´a:á:g
+                silent! %s:´e:é:g
+                silent! %s:´ı:í:g
+                silent! %s:´o:ó:g
+                silent! %s:´u:ú:g
+                silent! %s:˜n:ñ:g
+            ]]
 		end,
-		args={ nargs = '*', desc = 'Run the whole file' }
+		args={ nargs = '*', desc = 'Fix latex tildes' }
 	}
 }

ad558fb2f7ff161c4acd87a58ccaee9312c559bb
desmoris <damianesmoris@gmail.com>
2025-08-01 13:43:22 -0300

    fixed table concatenation method

--- lua/myconf/utils.lua
+++ lua/myconf/utils.lua
@@ -1,17 +1,17 @@
 local function copy_table(t)
-  local u = { } for k, v in pairs(t) do u[k] = v end
-  return setmetatable(u, getmetatable(t))
+  local u = { }
+  for k, v in pairs(t) do u[k] = v end
+  return u
 end
 
-local function concat_table_w_keys(t1,t2)
-	local new_table = copy_table(t1)
-	for key,value in pairs(t2) do new_table[key] = value end
-	return new_table
+local function concat_arr(t1, t2)
+	local u = copy_table(t1)
+	for _, v in pairs(t2) do table.insert(u, v) end
+	return u
 end
 
-local commands = {}
-commands = concat_table_w_keys(commands, require 'myconf.myplugins.runcode')
-commands = concat_table_w_keys(commands, require 'myconf.myplugins.fing-qol')
+commands = require 'myconf.myplugins.runcode'
+commands = concat_arr(commands, require 'myconf.myplugins.fing-qol')
 
 for _,command in pairs(commands) do
 	vim.api.nvim_create_user_command(command["name"], command["callback"], command["args"])

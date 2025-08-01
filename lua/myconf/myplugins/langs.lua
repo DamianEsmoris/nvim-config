55ec8cb34ce7a223de7bfa51eac773e96487c8ed
desmoris <damianesmoris@gmail.com>
2025-08-01 13:43:57 -0300

    added support to python

--- lua/myconf/myplugins/langs.lua
+++ lua/myconf/myplugins/langs.lua
@@ -1,13 +1,15 @@
 return {
 	run={
 		interpreted= {
-			javascript="if [ -f package.json ]; then npm i && npm run; else node _file; fi",
+			javascript="if [ -f package.json && _snippet -ne 1 ]; then npm i && npm run; else node _file; fi",
 			lua="lua _file",
 			php="[[ $(head -n1 _file) == \"<?php\" ]] && php _file || echo -e \"<?php\\n$(cat _file)\" | php",
-			sh="bash _file"
+			sh="bash _file",
+			python="[[ -f main.py && _snippet -ne 1 ]] && python main.py || python _file",
 		},
 		compiled= {
 			pascal="fpc _file && if [ -f _filename ]; then _filename; rm _filename.o; rm _filename; fi",
+            c="gcc _file -o _filename -lm && _filename; [ -f _filename ] && rm _filename;",
 			rust="if [ -f Cargo.toml ]; then cargo run; else rustc _file && _filename; [ -f _filename ] && rm _filename; fi"
 		}
 	},

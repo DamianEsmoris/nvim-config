f686f0203b8ad6a5a597296fea403727f467e936
desmoris <damianesmoris@gmail.com>
2025-08-01 13:40:13 -0300

    added lsp keymaps

--- lua/myconf/keymaps.lua
+++ lua/myconf/keymaps.lua
@@ -1,6 +1,6 @@
--- Helper functions to map keys in different modes
-function normal_mode(keymap, action)
-  vim.keymap.set("n", keymap, action)
+function normal_mode(keymap, action, opts)
+  opts = opts or {}
+  vim.keymap.set("n", keymap, action, opts)
 end
 
 function visual_mode(keymap, action)
@@ -33,3 +33,16 @@ vim.keymap.set("t", "<Esc>", "<C-\\><C-n>")
 normal_mode("<F5>", ":RunWholeFile<CR>")
 normal_mode("<F8>", ":TestWholeFile<CR>")
 visual_mode("<F5>", ":RunSnippet<CR>")
+
+-- LSP
+local opts = {buffer = bufnr, remap = false}
+normal_mode("gd", function() vim.lsp.buf.definition() end, opts)
+normal_mode("K", function() vim.lsp.buf.hover() end, opts)
+normal_mode("<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+normal_mode("<leader>vd", function() vim.diagnostic.open_float() end, opts)
+normal_mode("[d", function() vim.diagnostic.goto_next() end, opts)
+normal_mode("]d", function() vim.diagnostic.goto_prev() end, opts)
+normal_mode("<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+normal_mode("<leader>vrr", function() vim.lsp.buf.references() end, opts)
+normal_mode("<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+normal_mode("<C-h>", function() vim.lsp.buf.signature_help() end, opts)
